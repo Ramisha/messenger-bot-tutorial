e243afa17c9c1f963d614d583e09f7ab59dc13fa
Ramisha <gramishayohani@gmail.com>
2016-09-09 17:01:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,11 +34,7 @@ app.post('/webhook/', function (req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}
-			
+		
 			if (text === 'Yes') {
 			
 				conformation(sender)
