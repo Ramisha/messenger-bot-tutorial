dac13caeb3b5a86013b576d8c2ff907c0a277286
Ramisha <gramishayohani@gmail.com>
2016-09-09 12:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,25 @@ app.post('/webhook/', function (req, res) {
 })
 
 
+// testing :: get a date input 
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'create') {
+				sendCollectReq(sender)
+				continue
+			}
+			sendTextMessage(sender, "Hi, please use create or Generic key words , Not valid : " + text.substring(0, 200))
+		}
+	}
+	res.sendStatus(200)
+})
+
+
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.PAGE_ACCESS_TOKEN
 const token = "EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD"
@@ -73,6 +92,47 @@ function sendTextMessage(sender, text) {
 		}
 	})
 }
+
+
+// finction sendCollectReq :::: to collect and validate the date
+function sendCollectReq(sender){
+let messageData = {
+		  "message":{
+    "text":"Pick a color:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+  }
+}' "https://graph.facebook.com/v2.6/me/messages?access_token=token"    }
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+
+}
+
+// finction to send the image gallery with URL and buttons
 
 function sendGenericMessage(sender) {
 	let messageData = {
