dfe1ee67fd23c7ca30c1297d107a6ed743e58678
Ramisha <gramishayohani@gmail.com>
2016-09-13 17:52:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,15 +56,13 @@ app.post('/webhook/', function (req, res) {
 			
 			else {
 				
-			sendTextMessage(sender, "your destination is : " + text )
+			sendTextMessage(sender, "your destination is : " + text + "\n what is your departure location  ? " )
+			let departure = event.message.text
+			sendTextMessage(sender, "your departure location is : " + departure  + "\n \nwhen are you planning to leave ? " )
+			
 			continue
 			}
 			
-			continue
-			
-			sendTextMessage(sender, "what is your departure location  ?")
-			let departure = event.message.text
-			sendTextMessage(sender, "your departure location is : " + departure  + "\n \nwhen are you planning to leave ? " )
 			
 			
 		}
