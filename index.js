c74af2b3020800aceb05335e89d6344c36e1a4b6
Ramisha <gramishayohani@gmail.com>
2016-09-13 08:47:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,21 +34,11 @@ app.post('/webhook/', function (req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			
 			if (text === 'Generic') {
 				sendGenericMessage(sender)
 				continue
 			}
-			
-			if (text === 'Yes') {
-			
-				conformation(sender)
-				continue
-			}
-			
-			//sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-			sendTextMessage(sender, "Hey USER_ID I am an Itinerary recommender, If want to see ramdom itineries type Generic " + text.substring(0, 200))
-			//sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
@@ -84,158 +74,39 @@ function sendTextMessage(sender, text) {
 	})
 }
 
-// get user confirmation to continue
-function conformation(sender) {
-let messageData = { text:text }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-
-
-
 function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
 	
-let messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "airline_itinerary",
-        "intro_message": "Here\'s your flight itinerary.",
-        "locale": "en_US",
-        "pnr_number": "ABCDEF",
-        "passenger_info": [
-          {
-            "name": "Farbound Smith Jr",
-            "ticket_number": "0741234567890",
-            "passenger_id": "p001"
-          },
-          {
-            "name": "Nick Jones",
-            "ticket_number": "0741234567891",
-            "passenger_id": "p002"
-          }
-        ],
-        "flight_info": [
-          {
-            "connection_id": "c001",
-            "segment_id": "s001",
-            "flight_number": "KL9123",
-            "aircraft_type": "Boeing 737",
-            "departure_airport": {
-              "airport_code": "SFO",
-              "city": "San Francisco",
-              "terminal": "T4",
-              "gate": "G8"
-            },
-            "arrival_airport": {
-              "airport_code": "SLC",
-              "city": "Salt Lake City",
-              "terminal": "T4",
-              "gate": "G8"
-            },
-            "flight_schedule": {
-              "departure_time": "2016-01-02T19:45",
-              "arrival_time": "2016-01-02T21:20"
-            },
-            "travel_class": "business"
-          },
-          {
-            "connection_id": "c002",
-            "segment_id": "s002",
-            "flight_number": "KL321",
-            "aircraft_type": "Boeing 747-200",
-            "travel_class": "business",
-            "departure_airport": {
-              "airport_code": "SLC",
-              "city": "Salt Lake City",
-              "terminal": "T1",
-              "gate": "G33"
-            },
-            "arrival_airport": {
-              "airport_code": "AMS",
-              "city": "Amsterdam",
-              "terminal": "T1",
-              "gate": "G33"
-            },
-            "flight_schedule": {
-              "departure_time": "2016-01-02T22:45",
-              "arrival_time": "2016-01-03T17:20"
-            }
-          }
-        ],
-        "passenger_segment_info": [
-          {
-            "segment_id": "s001",
-            "passenger_id": "p001",
-            "seat": "12A",
-            "seat_type": "Business"
-          },
-          {
-            "segment_id": "s001",
-            "passenger_id": "p002",
-            "seat": "12B",
-            "seat_type": "Business"
-          },
-          {
-            "segment_id": "s002",
-            "passenger_id": "p001",
-            "seat": "73A",
-            "seat_type": "World Business",
-            "product_info": [
-              {
-                "title": "Lounge",
-                "value": "Complimentary lounge access"
-              },
-              {
-                "title": "Baggage",
-                "value": "1 extra bag 50lbs"
-              }
-            ]
-          },
-          {
-            "segment_id": "s002",
-            "passenger_id": "p002",
-            "seat": "73B",
-            "seat_type": "World Business",
-            "product_info": [
-              {
-                "title": "Lounge",
-                "value": "Complimentary lounge access"
-              },
-              {
-                "title": "Baggage",
-                "value": "1 extra bag 50lbs"
-              }
-            ]
-          }
-        ],
-        "price_info": [
-          {
-            "title": "Fuel surcharge",
-            "amount": "1597",
-            "currency": "USD"
-          }
-        ],
-        "base_price": "12206",
-        "tax": "200",
-        "total_price": "14003",
-        "currency": "USD"
-      }
-    }
-  }
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
@@ -252,8 +123,6 @@ let messageData = {
 		}
 	})
 }
-
-
 
 // spin spin sugar
 app.listen(app.get('port'), function() {
