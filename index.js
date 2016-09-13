b3e3bd200967e8c343a23ca3628729a5f073250a
Ramisha <gramishayohani@gmail.com>
2016-09-13 08:50:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,9 +54,137 @@ app.post('/webhook/', function (req, res) {
 // const token = process.env.PAGE_ACCESS_TOKEN
 const token = "EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD"
 
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
+
+function sendGenericMessage(sender) {
 	
+let messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "airline_itinerary",
+        "intro_message": "Here\'s your flight itinerary.",
+        "locale": "en_US",
+        "pnr_number": "ABCDEF",
+        "passenger_info": [
+          {
+            "name": "Farbound Smith Jr",
+            "ticket_number": "0741234567890",
+            "passenger_id": "p001"
+          },
+          {
+            "name": "Nick Jones",
+            "ticket_number": "0741234567891",
+            "passenger_id": "p002"
+          }
+        ],
+        "flight_info": [
+          {
+            "connection_id": "c001",
+            "segment_id": "s001",
+            "flight_number": "KL9123",
+            "aircraft_type": "Boeing 737",
+            "departure_airport": {
+              "airport_code": "SFO",
+              "city": "San Francisco",
+              "terminal": "T4",
+              "gate": "G8"
+            },
+            "arrival_airport": {
+              "airport_code": "SLC",
+              "city": "Salt Lake City",
+              "terminal": "T4",
+              "gate": "G8"
+            },
+            "flight_schedule": {
+              "departure_time": "2016-01-02T19:45",
+              "arrival_time": "2016-01-02T21:20"
+            },
+            "travel_class": "business"
+          },
+          {
+            "connection_id": "c002",
+            "segment_id": "s002",
+            "flight_number": "KL321",
+            "aircraft_type": "Boeing 747-200",
+            "travel_class": "business",
+            "departure_airport": {
+              "airport_code": "SLC",
+              "city": "Salt Lake City",
+              "terminal": "T1",
+              "gate": "G33"
+            },
+            "arrival_airport": {
+              "airport_code": "AMS",
+              "city": "Amsterdam",
+              "terminal": "T1",
+              "gate": "G33"
+            },
+            "flight_schedule": {
+              "departure_time": "2016-01-02T22:45",
+              "arrival_time": "2016-01-03T17:20"
+            }
+          }
+        ],
+        "passenger_segment_info": [
+          {
+            "segment_id": "s001",
+            "passenger_id": "p001",
+            "seat": "12A",
+            "seat_type": "Business"
+          },
+          {
+            "segment_id": "s001",
+            "passenger_id": "p002",
+            "seat": "12B",
+            "seat_type": "Business"
+          },
+          {
+            "segment_id": "s002",
+            "passenger_id": "p001",
+            "seat": "73A",
+            "seat_type": "World Business",
+            "product_info": [
+              {
+                "title": "Lounge",
+                "value": "Complimentary lounge access"
+              },
+              {
+                "title": "Baggage",
+                "value": "1 extra bag 50lbs"
+              }
+            ]
+          },
+          {
+            "segment_id": "s002",
+            "passenger_id": "p002",
+            "seat": "73B",
+            "seat_type": "World Business",
+            "product_info": [
+              {
+                "title": "Lounge",
+                "value": "Complimentary lounge access"
+              },
+              {
+                "title": "Baggage",
+                "value": "1 extra bag 50lbs"
+              }
+            ]
+          }
+        ],
+        "price_info": [
+          {
+            "title": "Fuel surcharge",
+            "amount": "1597",
+            "currency": "USD"
+          }
+        ],
+        "base_price": "12206",
+        "tax": "200",
+        "total_price": "14003",
+        "currency": "USD"
+      }
+    }
+  }
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
