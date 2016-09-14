2d0c581e0bfa0feb67e5890b664a545f0ef599c9
Ramisha <gramishayohani@gmail.com>
2016-09-14 18:01:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,14 @@ app.post('/webhook/', function (req, res) {
 			continue
 			}
 			
+				let varable = event.message.text
+				if (status === 'user_start') {
+			sendTextMessage(sender, "testing ")
+				sendTextMessage(sender, "varable ")
+				sendTextMessage(sender, "status ")
 		
-			
+			continue
+			}
 		
 	
 	
