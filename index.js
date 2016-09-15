19f8d58450517332141296e81a011158f1023834
Ramisha <gramishayohani@gmail.com>
2016-09-15 16:05:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,13 +96,13 @@ app.post('/webhook/', function (req, res) {
 			// use staus =========================== 
 		
 			
-			if (status === 'departure' && initiate !== 0) {
-		 	status = 'user_s_date';
-		 	sendTextMessage(sender, "your departure location is : " + initiate + "\n\nwhen are you planning to leave ?")
-		 	//let start_date = event.message.text
+			// if (status === 'departure' && initiate !== 0) {
+		 //	status = 'user_s_date';
+		 //	sendTextMessage(sender, "your departure location is : " + initiate + "\n\nwhen are you planning to leave ?")
+		 //	//let start_date = event.message.text
         		
-		 	continue
-		 	}
+		 //	continue
+		 //	}
 		 	
 		 	if (status === 'user_s_date') {
 			sendTextMessage(sender, "your departure date is : " + initiate + "\n\nwhen are you planning to return")
