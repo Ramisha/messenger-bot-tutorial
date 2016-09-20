9b781a66f675b6e6e9c32911328774c849d8fc1b
Bhathiya <hsbathiya@gmail.com>
2016-09-20 17:47:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/webhook/', function (req, res) {
 			 }
         break;
      case 'st_destination':
- 			if(initiate !== ''){
+ 			if(initiate.length>0){
 			  status = sendTextMessage(sender, "Your Destination is"  + initiate + "\nwhat is your origin ?", 'st_origin','st_destination')
 			 }
         break;
