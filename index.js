4a7f785b07c28e7732b9f30e924029f78b5d1aea
Bhathiya <hsbathiya@gmail.com>
2016-09-20 18:10:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,10 +61,9 @@ app.post('/webhook/', function (req, res) {
 			 }
         break;
      case 'st_destination':
-     		if(initiate === "LONDON"){
-			  sendTextMessage(sender, "Your Destination is"  + initiate + "\nwhat is your origin ?", 'st_origin','st_destination')
-			  status = 'st_origin';
-     		}
+     		status = 'st_origin';
+		sendTextMessage(sender, "Your Destination is"  + initiate + "\nwhat is your origin ?", 'st_origin','st_destination')
+
         break;
     default:status ='st_new_user'
 			
