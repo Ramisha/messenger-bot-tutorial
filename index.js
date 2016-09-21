f9fd985aa08ac11a81dd3496f40305257d40688e
Ramisha <gramishayohani@gmail.com>
2016-09-21 11:27:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import request from 'request'
+const MessengerApp = require('fb-messenger-app')
+var messenger = new MessengerApp(EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD)
 
-var FBMessenger = require('fb-messenger')
-var messenger = new FBMessenger(EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD)
+app.post('/webhook', function (req, res) {
+  var data = req.body
+  messenger._handleCallback(res, data)
+})
 
-messenger.sendTextMessage(973742352748403, 'Hello')
-
+messenger.sendApiMessage(USER_ID, {text: 'Howdy!'})
+ 
+var myImage = {
+  attachment:
+    { 
+      type: 'image',
+      payload: { 
+        url: 'https://petersapparel.com/img/shirt.png'
+      }
+    }
+  }
+ 
+messenger.sendApiMessage(USER_ID, myImage)
