1f0fce222fa7751b70f61773c91dd4c8d108e6b2
Ramisha <gramishayohani@gmail.com>
2016-09-21 15:53:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,18 @@ app.set('port', (process.env.PORT || 5000))
 
 var messenger = new MessengerApp("EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD")
 
+
+
+////////////////////////////// start :: webhook //////////////////////////////////
+
+
 app.post('/webhook', function (req, res) {
 
 	res.send('Error, wrong token')
   var data = req.body
   messenger._handleCallback(res, data)
   
-
+let messaging_events = req.body.entry[0].messaging;
 let event = req.body.entry[0].messaging[0]
 		let sender = event.sender.id
 messenger.sendApiMessage(sender, {text: 'Howdy!'})
@@ -37,9 +42,9 @@ var myImage = {
       }
     }
   }
-})// end of webhook
+})
 
-
+////////////////////////////// end :: webhook //////////////////////////////////
 //messenger.sendApiMessage(USER_ID, myImage)
 
 
