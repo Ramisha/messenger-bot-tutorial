83bdcc7158a7d1f45ffc37cbb37f9adf5b983fa4
Ramisha <gramishayohani@gmail.com>
2016-09-21 11:30:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const MessengerApp = require('fb-messenger-app')
 var messenger = new MessengerApp(EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD)
 
 app.post('/webhook', function (req, res) {
+  
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
   var data = req.body
   messenger._handleCallback(res, data)
 })
