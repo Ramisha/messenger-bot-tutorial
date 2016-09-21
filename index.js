e739455bfe0a7582770df0960f5d1e5202014bfb
Ramisha <gramishayohani@gmail.com>
2016-09-21 14:07:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const MessengerApp = require('fb-messenger')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+	res.send('testing ......')
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.set('port', (process.env.PORT || 5000))
+
 var messenger = new MessengerApp("EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD")
 
 app.post('/webhook', function (req, res) {
@@ -23,3 +37,12 @@ var myImage = {
   }
  
 messenger.sendApiMessage(USER_ID, myImage)
+
+
+
+
+
+
+app.listen(app.get('port'), function () {
+	console.log('running on port', app.get('port'))
+})
