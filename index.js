f98c6f1559877ee60460fb914211e53ce3abec24
Ramisha <gramishayohani@gmail.com>
2016-09-21 12:40:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MessengerApp = require('fb-messenger')
-var messenger = new MessengerApp(EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD)
+var messenger = new MessengerApp('EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD')
 
 app.post('/webhook', function (req, res) {
 
