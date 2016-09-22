ae026333df4941e90dd2f445181e3c38df90b0b5
Ramisha <gramishayohani@gmail.com>
2016-09-22 11:02:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var con_destination = '';
 var con_departure   = '';
 var con_end_date    = '';
 var con_start_date  = '';
+var name = '';
 
 const express    = require('express')
 const bodyParser = require('body-parser')
@@ -31,6 +32,20 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
+	
+	var fbMsngr = require('fb-msngr')({
+
+    access_token: 'EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD',
+   // notification_type: <notification_type>,
+    verify_token: 'my_voice_is_my_password_verify_me',
+    page_id: 973742352748403
+});
+
+
+fbMsngr.getProfile(id, function(err, first_name, last_name, profile_pic) {
+    name = first_name;
+});
+	
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
