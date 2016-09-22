210a7a2a31aa7e31ec7bd6ed07ad727fa0f2ba84
Ramisha <gramishayohani@gmail.com>
2016-09-22 08:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/webhook/', function (req, res) {
 
             if (initiate === 'HI' || initiate === 'HEY') {
                 sendTextMessage(sender, "type start over to continue creating your itinerary ")
-                button_check(sender)
+               // button_check(sender)
                 status = 'st_start';
             }
 
@@ -154,8 +154,8 @@ function sendTextMessage(sender, text) {
         }
     })
 }
-// get user confirmation to continue
-function sendItinerary(sender) {
+
+function sendItinerary(sender) {  // sample itinerary view 
 	
 let messageData = {
     "attachment":{
@@ -255,7 +255,7 @@ let messageData = {
 	
 	}
 
-function button_check(sender) {
+function button_check(sender) {  // sample button : not used 
 
     let messageData = {
         "attachment": {
@@ -289,153 +289,6 @@ function button_check(sender) {
             }
         }
     )
-}
-
-function sendGenericMessage(sender) {
-
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "airline_itinerary",
-                "intro_message": "Here\'s your flight itinerary.",
-                "locale": "en_US",
-                "pnr_number": "ABCDEF",
-                "passenger_info": [
-                    {
-                        "name": "Farbound Smith Jr",
-                        "ticket_number": "0741234567890",
-                        "passenger_id": "p001"
-                    },
-                    {
-                        "name": "Nick Jones",
-                        "ticket_number": "0741234567891",
-                        "passenger_id": "p002"
-                    }
-                ],
-                "flight_info": [
-                    {
-                        "connection_id": "c001",
-                        "segment_id": "s001",
-                        "flight_number": "KL9123",
-                        "aircraft_type": "Boeing 737",
-                        "departure_airport": {
-                            "airport_code": "SFO",
-                            "city": "San Francisco",
-                            "terminal": "T4",
-                            "gate": "G8"
-                        },
-                        "arrival_airport": {
-                            "airport_code": "SLC",
-                            "city": "Salt Lake City",
-                            "terminal": "T4",
-                            "gate": "G8"
-                        },
-                        "flight_schedule": {
-                            "departure_time": "2016-01-02T19:45",
-                            "arrival_time": "2016-01-02T21:20"
-                        },
-                        "travel_class": "business"
-                    },
-                    {
-                        "connection_id": "c002",
-                        "segment_id": "s002",
-                        "flight_number": "KL321",
-                        "aircraft_type": "Boeing 747-200",
-                        "travel_class": "business",
-                        "departure_airport": {
-                            "airport_code": "SLC",
-                            "city": "Salt Lake City",
-                            "terminal": "T1",
-                            "gate": "G33"
-                        },
-                        "arrival_airport": {
-                            "airport_code": "AMS",
-                            "city": "Amsterdam",
-                            "terminal": "T1",
-                            "gate": "G33"
-                        },
-                        "flight_schedule": {
-                            "departure_time": "2016-01-02T22:45",
-                            "arrival_time": "2016-01-03T17:20"
-                        }
-                    }
-                ],
-                "passenger_segment_info": [
-                    {
-                        "segment_id": "s001",
-                        "passenger_id": "p001",
-                        "seat": "12A",
-                        "seat_type": "Business"
-                    },
-                    {
-                        "segment_id": "s001",
-                        "passenger_id": "p002",
-                        "seat": "12B",
-                        "seat_type": "Business"
-                    },
-                    {
-                        "segment_id": "s002",
-                        "passenger_id": "p001",
-                        "seat": "73A",
-                        "seat_type": "World Business",
-                        "product_info": [
-                            {
-                                "title": "Lounge",
-                                "value": "Complimentary lounge access"
-                            },
-                            {
-                                "title": "Baggage",
-                                "value": "1 extra bag 50lbs"
-                            }
-                        ]
-                    },
-                    {
-                        "segment_id": "s002",
-                        "passenger_id": "p002",
-                        "seat": "73B",
-                        "seat_type": "World Business",
-                        "product_info": [
-                            {
-                                "title": "Lounge",
-                                "value": "Complimentary lounge access"
-                            },
-                            {
-                                "title": "Baggage",
-                                "value": "1 extra bag 50lbs"
-                            }
-                        ]
-                    }
-                ],
-                "price_info": [
-                    {
-                        "title": "Fuel surcharge",
-                        "amount": "1597",
-                        "currency": "USD"
-                    }
-                ],
-                "base_price": "12206",
-                "tax": "200",
-                "total_price": "14003",
-                "currency": "USD"
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: token},
-        method: 'POST',
-        json: {
-            recipient: {id: sender},
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
 }
 
 
