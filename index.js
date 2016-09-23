0b0c6a0868e36e0dfabf4cdecc142e9670aae641
Bhathiya <hsbathiya@gmail.com>
2016-09-23 12:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ app.get('/', function (req, res) {
     
 })
 
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
 
 const token = "EAAN1nQ8Jz3MBABpQib4sZB1UnMCIobDAQ7ArZA8w9U67AD1gimvvDCkLptz7k3keOTjZBY3DKZCyPIFZApIg3zn6I5ByFbNpQkwRfD99ZAejGmElK075ygLKJvHw4XWcb1ZCyY9V5gOkxgywVVhjZCWRCPPvBXdM5G1WykZCgcxSoPQZDZD"
 
