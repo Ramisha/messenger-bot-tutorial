19ac1f272294ebcf2b2c86dd80af5787ad1f5696
Ramisha <gramishayohani@gmail.com>
2016-09-23 08:52:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ The parameters are naturally passed through the req /foldername/file (/api/users
             console.log('Error: ', response.body.error)
         }
     })
-}*/
+}
 function testGet(callback) {
 
     return http.get({
@@ -210,6 +210,24 @@ function testGet(callback) {
         });
     });
 }
+*/
+
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
 
 function sendTextMessage(sender, text) {
     let messageData = {text: text}
