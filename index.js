63806faef17c2b56273ad6573fc394817971a1bf
Ramisha <gramishayohani@gmail.com>
2016-09-23 15:58:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function testGet(sender) {
             body += d;
         });
         response.on('end', function() {
-		console.log(body)
+		console.log(body[0].title)
 		sendTextMessage(sender, body[0].title)
             // Data reception is done, do whatever with it!
             var parsed = JSON.parse(body);
