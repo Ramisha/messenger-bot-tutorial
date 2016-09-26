8dfd496600ac56a4668c2ed835a4b5d2f7ab140c
Ramisha <gramishayohani@gmail.com>
2016-09-26 17:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/webhook/', function (req, res) {
             }
         }
         if (event.postback) {
-            let text = JSON.stringify(event.postback)
+            //let text = JSON.stringify(event.postback)
             sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
         }
     res.sendStatus(200)
