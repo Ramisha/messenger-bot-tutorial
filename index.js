d7d07e188eb91b8db0ab55e19cbe6fed35a1a1a4
Ramisha <gramishayohani@gmail.com>
2016-09-26 13:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,13 +51,43 @@ app.post('/webhook/', function (req, res) {
  //  console.log("\n*******\n"+JSON.stringify(res));
     console.log('\n************************************\n\n\n')
 
-	var request = require('request');
-request('http://jsonplaceholder.typicode.com/posts', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
-     }
-})
-
+//===========================
+    
+    
+    var optionsget = {
+    host : 'graph.facebook.com', // here only the domain name
+    // (no http/https !)
+   // port : 443,
+    path : '/youscada', // the rest of the url with parameters if needed
+    method : 'GET' // do GET
+};
+ 
+console.info('Options prepared:');
+console.info(optionsget);
+console.info('Do the GET call');
+ 
+// do the GET request
+var reqGet = https.request(optionsget, function(res) {
+    console.log("statusCode: ", res.statusCode);
+    // uncomment it for header details
+//  console.log("headers: ", res.headers);
+ 
+ 
+    res.on('data', function(d) {
+        console.info('GET result:\n');
+        process.stdout.write(d);
+        console.info('\n\nCall completed');
+    });
+ 
+});
+ 
+reqGet.end();
+reqGet.on('error', function(e) {
+    console.error(e);
+});
+    
+    
+//===========================
 
         if (event.message && event.message.text && !event.message.is_echo) {
             let initiate_temp = event.message.text
