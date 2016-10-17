7dd855416922b39a11f378268743fd8c84afcc33
Ramisha <gramishayohani@gmail.com>
2016-10-17 12:02:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ app.get('/', function (req, res) {
     console.log('initiated');
     //res.send('lol lol');
 })
-
-
-
 // to post data
 app.post('/webhook/', function (req, res) {
 
@@ -36,7 +33,7 @@ app.post('/webhook/', function (req, res) {
 	//var bid = JSON.stringify(results.toString());
 	//res.end(req.params.set-bid);  console.log(req.body.title);
 
-    var speech = "this is a webhook response for the bid" + bid;
+    var speech = "this is a webhook response for the bid  : " + results;
     var ret = {
         "speech": speech,
         "displayText": speech,
@@ -51,25 +48,7 @@ app.post('/webhook/', function (req, res) {
 	
     res.sendStatus(200)
 })
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.PAGE_ACCESS_TOKEN
 
-
-
-// routes will go here --------------------------- POST PARAMETERS ---------------------------
-
-
-// app.get('example.com', function(req, res) {
-//   var user_id = req.param('initiate');
-//   var token = req.param('con_departure');
-//   var geo = req.param('con_start_date');  
-
-//   res.send(user_id + ' ' + token + ' ' + geo);
-// });
-
-
-
-// spin spin sugar
 app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 })
